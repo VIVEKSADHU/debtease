3cf5256154648d7e0fb9970e246a3c117739ee03
Vivek <viveksadhu369@gmail.com>
2026-01-08 11:09:40 +0000

    Try fixing this error: `Runtime FirebaseError: Firebase: Error (auth/inv

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,11 @@
-// This file is intentionally left blank.
+
 // The Firebase configuration will be automatically provided during deployment.
-export const firebaseConfig = {};
+// For local development, this configuration is used.
+export const firebaseConfig = {
+  "projectId": "studio-8912600015-d3e57",
+  "appId": "1:611951108730:web:60c8744c875cf1fab0d1c3",
+  "apiKey": "AIzaSyDKS2BUzj2SDfLU0P75XiyWC4RTh5zlvI4",
+  "authDomain": "studio-8912600015-d3e57.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "611951108730"
+};
